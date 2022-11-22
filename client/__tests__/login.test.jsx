93784e0f273add708604246404f046446956a0bf
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-11-22 13:05:49 +0100

    updating snapshot

--- client/__tests__/login.test.jsx
+++ client/__tests__/login.test.jsx
@@ -3,26 +3,27 @@ import { act } from "@testing-library/react";
 import React from "react";
 import {Login} from "../login";
 import {BrowserRouter} from "react-router-dom";
+import {FrontPage} from "../frontpage";
 
 
-describe("ListMenu component", () => {
+describe("Frontpage component", () => {
 
-    it("Check if login contains login title", () => {
+    it("Check if frontpage contains login title", () => {
         const element = document.createElement("div");
 
         act(() => {
-            createRoot(element).render(<BrowserRouter><Login/></BrowserRouter>)
+            createRoot(element).render(<BrowserRouter><FrontPage/></BrowserRouter>)
         })
 
         expect(element.querySelector("h1").innerHTML).toEqual("Login")
 
     })
 
-    it("check if snapshot is matching \"Login\"", () => {
+    it("check if snapshot is matching \"Frontpage\"", () => {
         const element = document.createElement("div");
 
         act(() => {
-            createRoot(element).render(<BrowserRouter><Login/></BrowserRouter>)
+            createRoot(element).render(<BrowserRouter><FrontPage/></BrowserRouter>)
         })
 
         expect(element.innerHTML).toMatchSnapshot();
